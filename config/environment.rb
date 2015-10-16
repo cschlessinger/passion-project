6b24ebb5a140ba16b7c12d3c93a3e0ae75a7eef1
cschlessinger <claire.schlessinger@gmail.com>
2015-10-16 13:52:00 -0700

    update httparty case sensitivity

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
 # Require gems we care about
 require 'rubygems'
-require 'HTTParty'
+require 'httparty'
 require 'pp'
 require 'pry'
 require 'hirb'
