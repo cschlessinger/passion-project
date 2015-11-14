7a0e3d94b282f83f657c3e18441fe5d324c23dc1
cschlessinger <claire.schlessinger@gmail.com>
2015-11-14 12:50:15 -0800

    add api key

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,11 +4,9 @@ class Article < ActiveRecord::Base
   has_and_belongs_to_many :causes    
 
   def self.query_articles(keyword)
-    # @fq = "&fq=type_of_material:('News') AND section_name:('World')"
     url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=#{keyword}#{@fq}&sort=newest&api-key=#{ENV['NYTimesAPI']}"
   	response = HTTParty.get(url)
   	@articles = response["response"]['docs']
-  	# binding.pry
   	@articles
   end
 
@@ -17,14 +15,13 @@ class Article < ActiveRecord::Base
     p "*" * 100
     p keyword
   	all_keys = []
-  	# binding.pry
 		@ids = Article.pluck(:article_id) # Store all previous DB URLs in an array
+        binding.pry
   	Article.update_all(latest: 0)
   	@articles.each do |article|
   		article['keywords'].each do |keyword_collection|
 				all_keys << keyword_collection['value']
   		end
-      # @TODO: remove comments around this branching to renenable caching features
   		unless @ids.include?(article['_id'])
 	  		Article.create(article_id: article['_id'], url: article["web_url"], headline: article['headline']['main'], lead: article['lead_paragraph'], keywords: all_keys, latest: 1)
   		end
